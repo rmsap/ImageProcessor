a119e4b030c9231680514e397bf233941464b441
Micah Pac <pacis.m@northeastern.edu>
2022-06-03 17:00:27 -0400

    added view and controller interfaces

---
+++ src/controller/ImageProcessorController.java
@@ -0,0 +1,19 @@
+package controller;
+
+/**
+ * A controller for the Image Processor.
+ * The controller has one method that enables a client to utilize various commands.
+ * The controller enables the client to interact with the model and the view.
+ */
+public interface ImageProcessorController {
+
+
+  /**
+   * The method enables a client to send inputs to the model and outputs to the view.
+   * A client is able to call on various methods offered by the model for image processing.
+   */
+  public void execute();
+
+
+
+}
---
+++ src/view/ImageProcessorView.java
@@ -0,0 +1,24 @@
+package view;
+
+/**
+ * Represents the view of Image Processor.
+ * The view enables images to be displayed along with processing changes.
+ */
+public interface ImageProcessorView {
+
+  public String toString();
+
+
+  /**
+   * Renders the image.
+   * The method calls on the overridden toString() method to render the image.
+   */
+  public void renderImage();
+
+  /**
+   * Renders a given message to the view.
+   * @param message representing the String that is passed in.
+   */
+  public void renderMessage(String message);
+
+}
