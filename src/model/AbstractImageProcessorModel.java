fb78b28873e6e4c2d4a05de7bcbac5da62e75764
rmsap <87085375+rmsap@users.noreply.github.com>
2022-06-05 18:08:49 -0400

    Implemented model methods in child class

--- src/model/AbstractImageProcessorModel.java
+++ src/model/AbstractImageProcessorModel.java
@@ -1,8 +1,5 @@
 package model;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.HashMap;
 
 /**
@@ -23,32 +20,7 @@ public abstract class AbstractImageProcessorModel implements ImageProcessorModel
   public abstract void loadImage(String path, String name);
 
   @Override
-  public void saveImage(String path, String imageName) {
-    File imageFile = new File(path);
-    int[][] image = this.directory.get(imageName);
-
-    try {
-      imageFile.createNewFile();
-    } catch (IOException e) {
-      throw new IllegalArgumentException("File already exists.");
-    }
-
-    try {
-      FileWriter writer = new FileWriter(path);
-
-      // Write the PPM header into this file
-      writer.write("P3 ");
-
-      // Write all pixels from this image into the file
-      for (int[] pixel : image) {
-        writer.write(pixel[0] + " " + pixel[1] + " " + pixel[2] + "\n");
-      }
-
-      writer.close();
-    } catch (IOException e) {
-      throw new IllegalArgumentException("Failed to write to file.");
-    }
-  }
+  public abstract void saveImage(String path, String imageName);
 
   @Override
   public int[][] getImage(String name) {
--- src/model/PPMImageProcessorModel.java
+++ src/model/PPMImageProcessorModel.java
@@ -1,11 +1,47 @@
 package model;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * This class represents an ImageProcessorModel that works with PPM files.
  */
 public class PPMImageProcessorModel extends AbstractImageProcessorModel {
+  PPMImageProcessorModel() {
+    super();
+  }
+
   @Override
   public void loadImage(String path, String name) {
     directory.put(name, ImageUtil.readPPM(path));
+  }
+
+  @Override
+  public void saveImage(String path, String imageName) {
+    File imageFile = new File(path);
+    int[][] image = this.directory.get(imageName);
+
+    try {
+      imageFile.createNewFile();
+    } catch (IOException e) {
+      throw new IllegalArgumentException("File already exists.");
+    }
+
+    try {
+      FileWriter writer = new FileWriter(path);
+
+      // Write the PPM header into this file
+      writer.write("P3 ");
+
+      // Write all pixels from this image into the file
+      for (int[] pixel : image) {
+        writer.write(pixel[0] + " " + pixel[1] + " " + pixel[2] + "\n");
+      }
+
+      writer.close();
+    } catch (IOException e) {
+      throw new IllegalArgumentException("Failed to write to file.");
+    }
   }
 }
