e8856a5c204290e2177921b8db7e0ed4c8ea1f16
rmsap <87085375+rmsap@users.noreply.github.com>
2022-06-08 23:25:32 -0400

    Resolved merge conflicts.

--- src/controller/ImageProcessorController.java
+++ src/controller/ImageProcessorController.java
@@ -6,13 +6,11 @@ package controller;
  * The controller enables the client to interact with the model and the view.
  */
 public interface ImageProcessorController {
-
-
   /**
    * The method enables a client to send inputs to the model and outputs to the view.
    * A client is able to call on various methods offered by the model for image processing.
+   * @throws IllegalStateException if the controller fails to read from the Readable
+   *                               or write to the Appendable.
    */
   public void execute() throws IllegalStateException;
-
-
 }
