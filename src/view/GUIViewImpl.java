eee9afbd7cb74a0302a58ec6efd2436db4ca4da3
rmsap <87085375+rmsap@users.noreply.github.com>
2022-06-22 11:16:19 -0400

    Manipulated colors on histogram.

--- src/view/GUIViewImpl.java
+++ src/view/GUIViewImpl.java
@@ -164,7 +164,7 @@ public class GUIViewImpl extends JFrame implements ImageProcessorGUIView {
     JScrollPane imagePictureScrollPane = new JScrollPane() {
       @Override
       public Dimension getPreferredSize() {
-        return new Dimension(900, 300);
+        return new Dimension(900, 400);
       }
     };
 
@@ -179,7 +179,7 @@ public class GUIViewImpl extends JFrame implements ImageProcessorGUIView {
     JScrollPane histogramScrollPane = new JScrollPane() {
       @Override
       public Dimension getPreferredSize() {
-        return new Dimension(900, 300);
+        return new Dimension(900, 200);
       }
     };
 
@@ -201,11 +201,6 @@ public class GUIViewImpl extends JFrame implements ImageProcessorGUIView {
   @Override
   public void refresh(Image bruh) {
     ImageIcon image = new ImageIcon(bruh);
-//    for (int i = 0; i < 4; i++) {
-//      for (int j = 0; j < 4; j++) {
-//        System.out.println(new Color(bruh.getRGB(i,j)).getRed());
-//      }
-//    }
     this.imagePicture.setIcon(image); // putting the image in the JLabel
 
     // also need to refresh the histogram
@@ -286,11 +281,11 @@ public class GUIViewImpl extends JFrame implements ImageProcessorGUIView {
       }
     }
 
-    BufferedImage imageRed = new BufferedImage(256, 300, BufferedImage.TYPE_INT_ARGB);
-    BufferedImage imageGreen = new BufferedImage(256, 300, BufferedImage.TYPE_INT_ARGB);
-    BufferedImage imageBlue = new BufferedImage(256, 300, BufferedImage.TYPE_INT_ARGB);
+    BufferedImage imageRed = new BufferedImage(256, 200, BufferedImage.TYPE_INT_ARGB);
+    BufferedImage imageGreen = new BufferedImage(256, 200, BufferedImage.TYPE_INT_ARGB);
+    BufferedImage imageBlue = new BufferedImage(256, 200, BufferedImage.TYPE_INT_ARGB);
     BufferedImage imageIntensity = new BufferedImage(256,
-            300, BufferedImage.TYPE_INT_ARGB);
+            200, BufferedImage.TYPE_INT_ARGB);
 
     int maxValue = Math.max(Arrays.stream(redCounts).max().getAsInt(),
             Math.max(Arrays.stream(greenCounts).max().getAsInt(),
@@ -305,24 +300,24 @@ public class GUIViewImpl extends JFrame implements ImageProcessorGUIView {
     int range = maxValue - minValue;
 
     for (int i = 0; i < 256; i++) {
-      for (int j = 0; j < (int) ((redCounts[i] - minValue) / ((double) range) * 300); j++) {
-        imageRed.setRGB(i, 299 - j, new Color(100, 0, 0, 75).getRGB());
+      for (int j = 0; j < (int) ((redCounts[i] - minValue) / ((double) range) * 200); j++) {
+        imageRed.setRGB(i, 199 - j, new Color(100, 0, 0, 75).getRGB());
       }
 
-      for (int j = 0; j < (int) ((greenCounts[i] - minValue) / ((double) range) * 300); j++) {
-        imageGreen.setRGB(i, 299 - j, new Color(0, 100, 0, 75).getRGB());
+      for (int j = 0; j < (int) ((greenCounts[i] - minValue) / ((double) range) * 200); j++) {
+        imageGreen.setRGB(i, 199 - j, new Color(0, 100, 0, 75).getRGB());
       }
 
-      for (int j = 0; j < (int) ((blueCounts[i] - minValue) / ((double) range) * 300); j++) {
-        imageBlue.setRGB(i, 299 - j, new Color(0, 0, 100, 75).getRGB());
+      for (int j = 0; j < (int) ((blueCounts[i] - minValue) / ((double) range) * 200); j++) {
+        imageBlue.setRGB(i, 199 - j, new Color(0, 0, 100, 75).getRGB());
       }
 
-      for (int j = 0; j < (int) ((intensityCounts[i] - minValue) / ((double) range) * 300); j++) {
-        imageIntensity.setRGB(i, 299 - j, new Color(100, 100, 50, 75).getRGB());
+      for (int j = 0; j < (int) ((intensityCounts[i] - minValue) / ((double) range) * 200); j++) {
+        imageIntensity.setRGB(i, 199 - j, new Color(100, 100, 100, 20).getRGB());
       }
     }
 
-    BufferedImage histogram = new BufferedImage(256, 300, BufferedImage.TYPE_INT_ARGB);
+    BufferedImage histogram = new BufferedImage(256, 200, BufferedImage.TYPE_INT_ARGB);
 
     Graphics g = histogram.getGraphics();
     g.drawImage(imageRed, 0, 0, null);
